300d74fc96d00905e01b2a5dcb36120bcf0c1c5f
naveendevopsd15 <naveenmukkari03@gmail.com>
2024-12-30 16:55:06 +0530

    covesrsation added

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hi $PERSON1"
+echo "$PERSON1: How are doing $PERSON2?"
+echo "$PERSON2: I am good $PERSON1 how about you"
