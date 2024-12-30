fab9550a9565c15d42c2da79dcc115278c451ce6
naveendevopsd15 <naveenmukkari03@gmail.com>
2024-12-30 17:06:17 +0530

    covesrsation added

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter username:"
+read -s USERNAME
+echo "username entered is :$USERNAME"
+
+echo "Please enter Password:"
+read -s PASSWD
+echo "username entered is :$PASSWD"
