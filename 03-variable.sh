e4537f4df3504a4f33d6f9c21765faa2a86985ff
naveendevopsd15 <naveenmukkari03@gmail.com>
2024-12-30 16:49:12 +0530

    covesrsation added

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Raju
+PERSON2=Ravi
+
+echo "PERSON1: Hi PERSON2"
+echo "PERSON2: Hi PERSON1"
+echo "PERSON1: How are doing PERSON2?"
+echo "PERSON2: I am good PERSON1 how about you"
