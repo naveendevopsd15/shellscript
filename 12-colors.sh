8c19e2d07dcff6e07ccac77448ab9d26799c6738
naveendevopsd15 <naveenmukkari03@gmail.com>
2025-01-07 00:09:02 +0530

    colors shell

--- 12-colors.sh
+++ 12-colors.sh
@@ -7,7 +7,7 @@ N="\e[0m"
 
 VALIDATE (){
 
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
     then
     echo -e "$2 .... $R FAILURE" 
     exit 1
@@ -17,7 +17,7 @@ VALIDATE (){
 
 }
 
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then 
 echo "ERROR:: You must have sudo access to execute this script" 
 exit 1
@@ -25,7 +25,7 @@ fi
 
 dnf list installed mysql
 
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then
 dnf install mysql -y
 VALIDATE $? "Installing SQL"
