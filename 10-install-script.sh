3308ab0711a1258e841a3564bbfed3b3d7630f2d
naveendevopsd15 <naveenmukkari03@gmail.com>
2025-01-06 18:33:23 +0530

    conditions script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -5,7 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: You must have sudo access to execute this script"
-    # exit 1 #other than 0
+     exit 1 #other than 0
 fi
 dnf list installed mysql
 
@@ -22,3 +22,19 @@ then # not installed
 else
     echo "MySQL is already ... INSTALLED"
 fi
+
+dnf list installed git 
+
+if [ $? -ne 0]
+then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+     echo "Installing Git ... FAILURE"
+     exit 1
+else
+     echo "Installing Git ... SUCESS"
+fi
+else
+    echo "Git is alredy .... INSTALLED"
+fi    
